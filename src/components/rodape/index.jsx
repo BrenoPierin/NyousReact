162973e648d471320540383de40c29eca32505ee
unknown <brenomax02@gmail.com>
2020-10-21 01:46:13 -0300

    first commit

---
+++ src/components/rodape/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Rodape = () => {
+    return(
+        <footer className='text-center' style={{ marginTop : '70px'}} >
+            <h1>Senai de informatica 132</h1>
+            <small>Desenvolvido por <a href="https://github.com/BrenoPierin">Breno Pierin</a></small>
+
+        </footer>
+    )
+}
+
+export default Rodape;
